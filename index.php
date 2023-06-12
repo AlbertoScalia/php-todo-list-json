1dfe1248fa02551c106ab6383d642aa1f8ebd7e1
AlbertoScalia <albscalia@gmail.com>
2023-06-12 15:41:20 +0200

    Feat: Add Task function

--- index.php
+++ index.php
@@ -8,13 +8,41 @@
     <title>php-todo-list-json</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <style>
+        .container {
+            width: 50%;
+        }
+    </style>
 </head>
+
 <body>
+
     <div id="app">
-        <h1>ToDo List</h1>
+        <div class="container">
+            <h1 class="mt-4">ToDo List</h1>
+            <div class="card my-4">
+                <ul class="list-unstyled mb-0">
+                    <li v-for="task in tasks" class="p-3">
+                        {{task}}
+                    </li>
+                </ul>
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" @click="addTask()">
+                    +
+                </span>
+                <input type="text" class="form-control" placeholder="Add new task" v-model="newTask"
+                    @keyup.enter="addTask()">
+            </div>
+        </div>
     </div>
+
+
+
     <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
     <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
     <script src="./app.js"></script>
 </body>
+
 </html>
---
+++ storeTask.php
@@ -0,0 +1,22 @@
+<?php 
+
+if (isset($_POST["newTask"])) {
+    $task = $_POST["newTask"];
+
+    $tasksString = file_get_contents('tasks.json');
+
+    $tasksArray = json_decode($tasksString, true);
+
+    array_unshift($tasksArray, $task); 
+
+    $newTasksJSONString = json_encode($tasksArray);
+
+    file_put_contents('tasks.json', $newTasksJSONString);
+
+    header('Content-Type: application/json');
+
+    echo $newTasksJSONString; 
+}
+
+
+?>
